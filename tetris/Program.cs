c4aaebcc549eb26d5e1e7b225c3b75ae665614cb
FrankyGU12 <143767128+FrankyGU12@users.noreply.github.com>
2023-10-20 19:25:16 -0600

    TETRIS GIRATORIO
    TETRIS

--- tetris/Program.cs
+++ tetris/Program.cs
@@ -49,6 +49,9 @@ class Tetris
                             return;
                         }
                         break;
+                    case ConsoleKey.UpArrow:
+                        RotateTetromino();
+                        break;
                 }
             }
 
@@ -166,7 +169,76 @@ class Tetris
 
         return true;
     }
+
+    static void RotateTetromino()
+    {
+        char[,] rotatedTetromino = new char[tetrominoShape.GetLength(1), tetrominoShape.GetLength(0)];
+
+        for (int y = 0; y < tetrominoShape.GetLength(0); y++)
+        {
+            for (int x = 0; x < tetrominoShape.GetLength(1); x++)
+            {
+                rotatedTetromino[x, y] = tetrominoShape[y, tetrominoShape.GetLength(1) - 1 - x];
+            }
+        }
+
+        if (IsRotationValid(rotatedTetromino))
+        {
+            // Borra la posición actual del tetromino en el tablero.
+            for (int y = 0; y < tetrominoShape.GetLength(0); y++)
+            {
+                for (int x = 0; x < tetrominoShape.GetLength(1); x++)
+                {
+                    if (tetrominoShape[y, x] == 'O')
+                    {
+                        board[y + currentY, x + currentX] = ' ';
+                    }
+                }
+            }
+
+            // Actualiza la forma del tetromino rotado.
+            tetrominoShape = rotatedTetromino;
+
+            // Dibuja el tetromino en la nueva posición.
+            for (int y = 0; y < tetrominoShape.GetLength(0); y++)
+            {
+                for (int x = 0; x < tetrominoShape.GetLength(1); x++)
+                {
+                    if (tetrominoShape[y, x] == 'O')
+                    {
+                        board[y + currentY, x + currentX] = tetromino;
+                    }
+                }
+            }
+
+            // Dibuja el tablero actualizado.
+            DrawBoard();
+        }
+    }
+
+    static bool IsRotationValid(char[,] rotatedTetromino)
+    {
+        for (int y = 0; y < rotatedTetromino.GetLength(0); y++)
+        {
+            for (int x = 0; x < rotatedTetromino.GetLength(1); x++)
+            {
+                if (rotatedTetromino[y, x] == 'O')
+                {
+                    int boardX = x + currentX;
+                    int boardY = y + currentY;
+
+                    if (boardX < 0 || boardX >= boardWidth || boardY >= boardHeight || boardY < 0 || boardX < 0)
+                    {
+                        return false; // Rotación inválida
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
 }
+
 
 
 
